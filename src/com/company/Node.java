00419a00a167fb74f3106a563ba6f921c5faeb8b
한재현 <sdsd4564@gmail.com>
2017-04-18 00:49:03 +0900

    170418 Travel Salesman Problem by Greedy Algorith

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -1,19 +1,16 @@
 package com.company;
 
-/**
- * Created by USER on 2017-04-17.
- */
-class Node {
+class Node {                // 포인트 객체 클래스
     int x = 0;
     int y = 0;
     double distance = 0.0;
 
-    Node(int x, int y) {
+    Node(int x, int y) {    // x와 y 좌표를 입력받기 위한 생성자
         this.x = x;
         this.y = y;
     }
 
-    double getDistance(Node target) {
+    double getDistance(Node target) { // 목표 포인트 객체까지의 거리를 리턴함.
         return Math.sqrt(Math.pow(target.x - x, 2) + (Math.pow(target.y - y, 2)));
     }
 }
